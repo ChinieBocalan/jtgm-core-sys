8b3b327c1e43d4d3452291358b5af590f32cfc12
praiseejoybocalan <uritoong@gmail.com>
2025-01-19 17:56:36 +0800

    Skeleton Code for Attendance.

---
+++ src/main/java/org/jtgm/sys/presentation/AttendanceController.java
@@ -0,0 +1,13 @@
+package org.jtgm.sys.presentation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/jtgm/health-check")
+@Slf4j
+@RequiredArgsConstructor
+public class AttendanceController {
+
+}
