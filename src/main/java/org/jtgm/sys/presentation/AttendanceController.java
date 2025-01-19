79d9460d8f7754ee42aab205e574855604f38b42
PHINE <leon.josephine2002@gmail.com>
2025-01-19 19:51:16 +0800

    DTO

--- src/main/java/org/jtgm/sys/presentation/AttendanceController.java
+++ src/main/java/org/jtgm/sys/presentation/AttendanceController.java
@@ -3,8 +3,10 @@ package org.jtgm.sys.presentation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jtgm.sys.core.dto.AttendanceDTO;
+import org.jtgm.sys.core.dto.HealthCheckDTO;
 import org.jtgm.sys.core.exception.GenericServiceErrorException;
 import org.jtgm.sys.core.service.AttendanceService;
+import org.jtgm.sys.core.service.HealthCheckService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +20,12 @@ public class AttendanceController {
     @PostMapping(path="/update", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AttendanceDTO> updateSystemHealth(@RequestBody AttendanceDTO attendanceDTO) {
         try {
-            log.info("[INFO] Attendance Update Started");
-            AttendanceService attendanceService;
+            log.info("[INFO] Attendee Update Started");
             attendanceService.update(attendanceDTO);
             log.info("[END] Successfully updated the health check details.");
             return ResponseEntity.ok(attendanceDTO);
         } catch (Exception e){
             throw new GenericServiceErrorException("Failed to reach the database", e);
+        }
+    }
 }
