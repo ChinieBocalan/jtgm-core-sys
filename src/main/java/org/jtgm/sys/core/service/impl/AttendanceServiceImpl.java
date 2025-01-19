44256365d9e0e558b18354f43c721c3d0fa30260
Uritoong <Uritoong@gmail.com>
2025-01-19 17:57:46 +0800

    Skeleton Code for Attendance. (#12)

---
+++ src/main/java/org/jtgm/sys/core/service/impl/AttendanceServiceImpl.java
@@ -0,0 +1,12 @@
+package org.jtgm.sys.core.service.impl;
+
+
+import lombok.RequiredArgsConstructor;
+import org.jtgm.sys.core.service.AttendanceService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AttendanceServiceImpl implements AttendanceService {
+
+}
---
+++ src/main/java/org/jtgm/sys/repository/AttendanceRepository.java
@@ -0,0 +1,5 @@
+package org.jtgm.sys.repository;
+
+public interface AttendanceRepository {
+
+}
