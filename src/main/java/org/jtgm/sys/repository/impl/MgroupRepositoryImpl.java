3774f2efbac5ebf87181d8c2a39ed8594cd2fc64
laarni10 <laarni@integr8.uk>
2025-01-24 22:19:10 +0800

    Update the Get Method parameters

--- src/main/java/org/jtgm/sys/repository/impl/MgroupRepositoryImpl.java
+++ src/main/java/org/jtgm/sys/repository/impl/MgroupRepositoryImpl.java
@@ -2,11 +2,11 @@ package org.jtgm.sys.repository.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jtgm.sys.core.dto.HealthCheckDTO;
-import org.jtgm.sys.repository.HealthCheckRepository;
-import org.jtgm.sys.repository.entity.healthcheck.HealtchCheckEntity;
+import org.jtgm.sys.core.dto.MgroupDTO;
+import org.jtgm.sys.repository.MgroupRepository;
+import org.jtgm.sys.repository.entity.mgroup.MGroupEntity;
 import org.jtgm.sys.repository.exception.GenericRepositoryErrorException;
-import org.jtgm.sys.repository.jpa.HealthCheckJpa;
+import org.jtgm.sys.repository.jpa.MgroupServiceJpa;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -17,43 +17,17 @@ import java.util.List;
 @Slf4j
 public class MgroupRepositoryImpl implements MgroupRepository {
 
-    final private HealthCheckJpa healthCheckJpa;
+    final private MgroupServiceJpa mgroupServiceJpa;
 
     @Override
-    public HealtchCheckEntity getHealthCheckDetails() {
+    public MGroupEntity getMGroupDetails() {
         try {
             log.info("[INFO] Retrieving details from database");
-            HealtchCheckEntity healtchCheckEntity = healthCheckJpa.findByIsActive(1).get();
+            MGroupEntity mGroupEntity = mgroupServiceJpa.findByIsActive(1).get();
             log.info("[INFO] Retrieving details from database");
-            return healtchCheckEntity;
+            return mGroupEntity;
         }catch (Exception e){
             throw new GenericRepositoryErrorException("[ERROR] Failed to retrieve details", e);
         }
-    }
-
-    @Override
-    public HealtchCheckEntity update(HealthCheckDTO healthCheckDTO) {
-        HealtchCheckEntity healtchCheckEntity = new HealtchCheckEntity().builder()
-                .sysName(healthCheckDTO.getSysName())
-                .sysDesc(healthCheckDTO.getSysDesc())
-                .version(healthCheckDTO.getVersion())
-                .date(LocalDateTime.now())
-                .isActive(1)
-                .build();
-
-        List<HealtchCheckEntity> healtchCheckEntityList = healthCheckJpa.findAll();
-        healtchCheckEntityList
-                .stream()
-                .map(f -> HealtchCheckEntity.builder()
-                        .id(f.getId())
-                        .sysName(f.getSysName())
-                        .sysDesc(f.getSysDesc())
-                        .version(f.getVersion())
-                        .date(f.getDate())
-                        .isActive(0)
-                        .build()
-                ).forEach(healthCheckJpa::save);
-
-        return healthCheckJpa.save(healtchCheckEntity);
     }
 }
